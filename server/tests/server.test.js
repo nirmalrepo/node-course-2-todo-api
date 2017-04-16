35cdc60c75e54b02bc688d443c89f9519aa2ab68
nirmalwsolus <nirmal@wsolus.com>
2017-04-16 20:06:56 +0530

    add test cases for GET /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -3,6 +3,7 @@
  */
 const expect=require('expect');
 const request=require('supertest');
+const {ObjectID}=require('mongodb');
 
 const {app}=require('./../server');
 const {Todo}=require('./../models/todo');
@@ -10,9 +11,11 @@ const {Todo}=require('./../models/todo');
 
 const todos= [
         {
+            _id:new ObjectID(),
             "text": "First test todo",
         },
         {
+            _id:new ObjectID(),
             "text": "Second test todor",
         }
     ]
@@ -72,9 +75,36 @@ describe('GET /todos',()=>{
     it('should get all todos',(done)=>{
         request(app)
             .get('/todos')
-            .send({}) //empty data
             .expect(200)
             .expect((res)=>{expect(res.body.todos.length).toBe(2)})
+            .end(done);
+    });
+});
+
+describe('GET /todos/:id',()=>{
+    it('should return todo doc',(done)=>{
+        request(app)
+            .get(`/todos/${todos[0]._id.toHexString()}`)
+            .expect(200)
+            .expect((res)=>{
+                expect(res.body.todo.text).toBe(todos[0].text)
+            })
+            .end(done);
+    });
+
+    it('should return 404 if todo not found',(done)=>{
+        var hexId=new ObjectID().toHexString();
+        request(app)
+            .get(`/todos/${hexId}`)
+            .expect(404)
+
+            .end(done);
+    });
+
+    it('should return 404 for non-object ids',(done)=>{
+        request(app)
+            .get(`/todos/345345345534`)
+            .expect(404)
             .end(done);
     });
 });
