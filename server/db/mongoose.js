85bfe165074c2a233ff6ac5373d085c9db0b6cf5
nirmalwsolus <nirmal@wsolus.com>
2017-04-17 09:08:00 +0530

    adding heroku db conncetion to db/mongoose.js

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -7,6 +7,6 @@ var mongoose=require('mongoose');
 mongoose.Promise=global.Promise;
 
 //db connection
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
 module.exports={mongoose};
